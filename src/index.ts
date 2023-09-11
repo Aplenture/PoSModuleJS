c51b4adb6e2d156c0cbcb4f18b2a305276d6873b
fweise <public@weise-games.de>
2023-09-11 17:43:10 +0200

    update frameworks

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ export class Module extends BackendJS.Module.Module<Context, Args, Options> impl
         this.database = new BackendJS.Database.Database(this.options.databaseConfig, args.debug);
         this.database.onMessage.on(message => this.onMessage.emit(this, `database '${this.options.databaseConfig.database}' ${message}`));
 
-        this.myRepository = new MyRepository(options.databaseTable, this.database);
+        this.myRepository = new MyRepository(options.databaseTable, this.database, __dirname + '/updates/MyRepository');
 
         this.addCommands(Object.values(require('./commands')).map((constructor: any) => new constructor(this)));
     }
